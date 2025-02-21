98905aabeecc68e5313c4f1975585eecac2baeac
wdjohanwd <wdjohanwd@gmail.com>
2025-02-21 13:24:08 +0100

    .

--- react-project/src/components/Error404.jsx
+++ react-project/src/components/Error404.jsx
@@ -3,8 +3,8 @@ const Error404 = () => {
         <div id='oopss'>
             <div id='error-text'>
                 <img src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg" alt="404"/>
-                    <span> That page isn&apos;t avaliable</span>
-                    <p className="p-a">We are very sorry, please try go back home</p>
+                    <span> That page isn&apos;t available</span>
+                    <p className="p-a">We are very sorry, please try going back home</p>
                     {/* <p className="p-b">We sorry,  </p> */}
                     <a href='/' className="back">Go home</a>
             </div>
